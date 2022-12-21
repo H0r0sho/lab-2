a524fc6cfd848d73543e4cb2a1ce70d7a37db141
H0r0sho <undeaddragon163rus@gmail.com>
2022-12-22 03:13:54 +0400

    added s3, s4, s5

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from s1 import run_1
 from s2 import run_2
-from s3 import run_3
-
-
-
+from s3 import write_to_file
+from s32 import run_3
+from s4 import run_4
 
 if __name__ == '__main__':
-    run_1()
-    run_2()
-    run_3()
+    #run_1()
+    #run_2()
+    #write_to_file()
+    #run_3()
+    run_4()
+
--- s1.py
+++ s1.py
@@ -2,27 +2,21 @@ import csv
 import os
 
 
-def make_dir(path_fol: str, path_sc1: str) -> None:
-    if not os.path.isdir(path_fol): os.mkdir(path_fol)
-    if not os.path.isdir(path_sc1): os.mkdir(path_sc1)
-
-def run_1(path_to_csv: str=os.path.join("C:/", "Users", "79171", "PyCharmProjects", "lab2")) -> None:
-    path_fol, path_sc1 = "lab2", "lab2/s1"
-    make_dir(path_fol, path_sc1)
+def run_1(path_to_csv: str=os.path.join("C:/", "Users", "79171", "PyCharmProjects")) -> None:
     list1 = []
-    with open(path_to_csv + '/dataset.csv', 'r', encoding='utf-8') as csvfile:
+    with open(path_to_csv + '/lab2/dataset.csv', 'r', encoding='utf-8') as csvfile:
         file_reader = csv.reader(csvfile)
         for row in file_reader:
             list1.append(row)
 
     with open(path_to_csv + "/lab2/X.csv", 'w', newline='', encoding='utf-8') as csvfile_x:
-        for i in reversed(range(0, len(list1))):
-            all_data = [list1[i][0][0:10]]
+        for i in (range(0, len(list1))):
+            all_data = [list1[i][0]]
             writer = csv.writer(csvfile_x)
             writer.writerow(all_data)
 
     with open(path_to_csv + "/lab2/Y.csv", 'w', newline='', encoding='utf-8') as csvfile_y:
         for i in range(0, len(list1)):
-            all_data = [list1[i][0][12:]]
+            all_data = [list1[i][1]]
             writer = csv.writer(csvfile_y)
             writer.writerow(all_data)
